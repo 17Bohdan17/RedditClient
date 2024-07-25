d037767e89ad903bf89fe82589ba43fa2a16edb8
17Bohdan17 <bogdanchirkov.del@gmail.com>
2024-07-25 23:53:15 +0300

    RC 1-5 Add possibility to save image in gallery

--- app/src/main/java/com/mycompany/redditclient/MainActivity.kt
+++ app/src/main/java/com/mycompany/redditclient/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.mycompany.redditclient
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.Manifest
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import java.io.OutputStream
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,49 +32,70 @@ class MainActivity : AppCompatActivity() {
     private lateinit var postsAdapter: PostsAdapter
     private var after: String? = null
     private var isLoading = false
+    private var currentPage = 1
 
+    private val REQUEST_WRITE_STORAGE = 1
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            checkPermissions()
+        } else {
+            initRecyclerView()
+            fetchTopPosts()
+        }
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQUEST_WRITE_STORAGE
+            )
+        } else {
+            initRecyclerView()
+            fetchTopPosts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_WRITE_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initRecyclerView()
+                    fetchTopPosts()
+                } else {
+                    Toast.makeText(this, "Permission required to save images", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        fetchTopPosts()
-
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-                if (!isLoading && layoutManager.findLastCompletelyVisibleItemPosition() == postsAdapter.itemCount - 1) {
-                    isLoading = true
-                    loadMore()
-                }
-            }
-        })
     }
 
     private fun fetchTopPosts(after: String? = null) {
         val call = RetrofitInstance.api.getTopPosts(after)
 
-        call.enqueue(object: Callback<RedditResponse> {
+        call.enqueue(object : Callback<RedditResponse> {
             override fun onResponse(call: Call<RedditResponse>, response: Response<RedditResponse>) {
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     response.body()?.data?.children?.let { redditChildren ->
                         val posts = redditChildren.map { it.data }
                         if (after == null) {
-                            postsAdapter = PostsAdapter(posts.toMutableList())
+                            postsAdapter = PostsAdapter(posts.toMutableList(), this@MainActivity)
                             recyclerView.adapter = postsAdapter
                         } else {
-                            val positionStart = (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-                            val topView = recyclerView.getChildAt(0)
-                            val topOffset = topView?.top ?: 0
-
                             postsAdapter.addPosts(posts)
-                            recyclerView.scrollToPosition(positionStart)
-                            recyclerView.scrollBy(0, topOffset)
                         }
                         this@MainActivity.after = response.body()?.data?.after
                     }
@@ -73,7 +105,7 @@ class MainActivity : AppCompatActivity() {
                 isLoading = false
             }
 
-            override fun onFailure(call: Call<RedditResponse>, t: Throwable){
+            override fun onFailure(call: Call<RedditResponse>, t: Throwable) {
                 Log.e("MainActivity", "Error fetching posts", t)
                 isLoading = false
             }
@@ -81,9 +113,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadMore() {
-        fetchTopPosts(after)  // Вызов функции для получения следующей страницы с параметром `after`
+        fetchTopPosts(after)
+    }
+
+    fun saveImageToGallery(imageUrl: String) {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(imageUrl).build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                Log.e("MainActivity", "Failed to download image", e)
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Failed to download image", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                response.body()?.let { responseBody ->
+                    val inputStream = responseBody.byteStream()
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    inputStream.close()
+
+                    saveBitmapToGallery(bitmap)
+                }
+            }
+        })
+    }
+
+    private fun saveBitmapToGallery(bitmap: Bitmap) {
+        val filename = "${System.currentTimeMillis()}.jpg"
+        val outputStream: OutputStream?
+        val contentResolver = contentResolver
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+        }
+
+        val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        outputStream = imageUri?.let { contentResolver.openOutputStream(it) }
+
+        outputStream?.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            runOnUiThread {
+                Toast.makeText(this, "Image saved to gallery", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
+
 
 
 data class RedditResponse(
@@ -107,3 +187,4 @@ data class RedditPost(
     val num_comments: Int,
     val url: String,
 )
+
